1f5d18b2474d83b47492d1f59a3181e0fe246bce
kamrul1717 <kamrulhassan1361@gmail.com>
2021-07-09 23:41:05 +0600

    store crawled data into diff files and start the sqlite

--- quick_crawler/spiders/quotes_spider.py
+++ quick_crawler/spiders/quotes_spider.py
@@ -1,4 +1,5 @@
 import scrapy;
+from ..items import QuickCrawlerItem
 
 class QuoteSpider(scrapy.Spider):
 	name = 'quotes'
@@ -7,6 +8,19 @@ class QuoteSpider(scrapy.Spider):
 	]
 
 	def parse(self, response):
-		title = response.css('title::text').extract();
-		yield {'titletext':title}
+		# title = response.css('title::text').extract()
+		# yield {'titletext':title}
+
+		items = QuickCrawlerItem();
+
+		all_div_quotes = response.css("div.quote")
+
+		for quote in all_div_quotes:
+			title = quote.css('span.text::text').extract()
+			author = quote.css('.author::text').extract()
+			tag = quote.css('.tag::text').extract()
+			items['title'] = title
+			items['author'] = author
+			items['tag'] = tag
+			yield items
 
