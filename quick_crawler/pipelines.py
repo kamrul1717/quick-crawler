ec7eaed3b34615e5e392aa6a1888947331479749
kamrul1717 <kamrulhassan1361@gmail.com>
2021-07-10 00:19:52 +0600

    stored data in sqlite dataabse

--- quick_crawler/pipelines.py
+++ quick_crawler/pipelines.py
@@ -7,8 +7,26 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+import sqlite3
 
 class QuickCrawlerPipeline:
-    def process_item(self, item, spider):
-    	print("Pipeline Data : "+item['title'][0])
-    	return item
+	def __init__(self):
+		self.create_connection()
+		self.create_table()
+
+	def create_connection(self):
+		self.conn = sqlite3.connect('my_quotes.db')
+		self.curr = self.conn.cursor()
+
+	def create_table(self):
+		self.curr.execute("""DROP TABLE IF EXISTS quotes_tb""")
+		self.curr.execute("""DROP TABLE IF EXISTS quotes""")
+		self.curr.execute("""create table quotes( title text, author text, tag text)""")
+
+	def process_item(self, item, spider):
+		self.store_db(item)
+		return item
+
+	def store_db(self, item):
+		self.curr.execute("""insert into quotes values (?,?,?) """, (item['title'][0], item['author'][0], item['tag'][0]))
+		self.conn.commit()
